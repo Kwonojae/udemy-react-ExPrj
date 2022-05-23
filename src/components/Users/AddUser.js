83d4f2573620e07382e2ed62c8daa5e79c691343
Ojea <rnjsdhwo1325@gmail.com>
2022-05-24 05:23:14 +0900

    event handler add

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   //유저 추가
   const addUserHandler = (event) => {
     event.preventDefault();
     //submit 이벤트가 실행되도록 요청하는 기본적으로 정의된 동작을 중단하도록함 .
+    console.log(enteredUsername, enteredAge);
+  };
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="text" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
         <Button type="submit">Add user</Button>
       </form>
     </Card>
