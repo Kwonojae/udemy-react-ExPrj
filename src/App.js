6e29eceea7fad204ffac057a41b169e944cc7b90
Ojea <rnjsdhwo1325@gmail.com>
2022-05-25 12:50:15 +0900

    UserList add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    //추가한 유저를 받아와서 UserList로 보내서 화면에 보여주게 함
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,8 +7,8 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
-  //유저 추가
   const addUserHandler = (event) => {
+    //유저 추가 함수
     event.preventDefault();
     //submit 이벤트가 실행되도록 요청하는 기본적으로 정의된 동작을 중단하도록함 .
     //trim 은 문자열의 양 끝의 앞뒤 공백을 제거한다
@@ -19,7 +19,8 @@ const AddUser = (props) => {
       //+enteredAge 문자열로 받아온것을 숫자형으로 바꿔준다
       return;
     }
-    console.log(enteredUsername, enteredAge);
+    //추가한 유저를 보내주는 역활을함.
+    props.onAddUser(enteredUsername, enteredAge);
     setEnteredUsername("");
     setEnteredAge("");
   };
